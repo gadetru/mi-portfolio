9cd1e99f3e1a8ba0f34d9a2dda79b50639624a93
Gabriel Delgado <gadetru@gmail.com>
2023-11-16 13:53:50 +0100

    'mejoras-experiencia'

--- src/components/experiencia/Experiencia.jsx
+++ src/components/experiencia/Experiencia.jsx
@@ -4,6 +4,50 @@ import "./experiencia.scss";
 export const Experiencia = () => {
   return (
     <div className="caja-experiencia">
+
+      <h2>Experiencia:</h2>
+      <div className="lugares">
+        <h3>Nükrum Technologies</h3>
+        <ul>
+          <li>
+            Creación del proyecto digital Nükrum basado en tratamientos médicos
+            innovadores.
+          </li>
+          <li>Tecnologías: HTML,SASS,NodeJS,React,MySQL,Azure,Bitbucket,figma,</li>
+        </ul>
+      </div>
+      <div className="lugares">
+        <h3>AIcrop</h3>
+
+        <ul>
+          <li>
+            Creación del proyecto digital de AIcrop parael control automatizado
+            de invernaderos.
+          </li>
+
+          <li>
+            Desarrollo y maquetación fronted Implementación de la lógica y
+            peticiones del servidor.
+          </li>
+          <li>Gestión y diseño de la base de datos.</li>
+          <li>Tecnologías: HTML,SASS,NodeJS,React,MySQL,GitHub,figma,SCRUM,</li>
+        </ul>
+      </div>
+      <div className="idiomas">
+              <h2>Idiomas:</h2>
+      <div>
+        <h3>Castellano</h3>
+        <p>Lengua materna</p>
+      </div>
+      <div>
+        <h3>Alemán.</h3>
+        <p>Nivel B2 ,experiencia laboral en el país de 7 años.</p>
+      </div>
+      <div>
+        <h3>Inglés</h3>
+        <p>Nivel básico.</p>
+      </div>
+      </div>
       <h2>Formación:</h2>
 
       <div className="mi-formacion">
@@ -24,48 +68,6 @@ export const Experiencia = () => {
           <li>Desarrollo de proyectos utilizando metodología Agile ySCRUM.</li>
         </ul>
       </div>
-      <h2>Experiencia:</h2>
-      <div className="lugares">
-        <h3>Nükrum Technologies</h3>
-        <ul>
-          <li>
-            Creación del proyecto digital Nükrum basado en tratamientos médicos
-            innovadores.
-          </li>
-        </ul>
-      </div>
-      <div className="lugares">
-        <h3>AIcrop</h3>
-
-        <ul>
-          <li>
-            Creación del proyecto digital de AIcrop parael control automatizado
-            de invernaderos.
-          </li>
-
-          <li>
-            Desarrollo y maquetación fronted Implementación de la lógica y
-            peticiones del servidor.
-          </li>
-          <li>Gestión y diseño de la base de datos.</li>
-        </ul>
-      </div>
-      <div className="idiomas">
-              <h2>Idiomas:</h2>
-      <div>
-        <h3>Castellano</h3>
-        <p>Lengua materna</p>
-      </div>
-      <div>
-        <h3>Alemán.</h3>
-        <p>Nivel B2 ,experiencia laboral en el país de 7 años.</p>
-      </div>
-      <div>
-        <h3>Inglés</h3>
-        <p>Nivel básico.</p>
-      </div>
-      </div>
-
     </div>
   );
 };
--- src/components/filtro/Filtrado.jsx
+++ src/components/filtro/Filtrado.jsx
@@ -13,7 +13,7 @@ export const Filtrado = ({onFilterChange}) => {
 
   return (
     <div className='botonera-filtro'>
-      <button className={filtro === 'vanilla'? 'activa' : ''} onClick={() => handleFiltroChange("vanilla")}>Vanilla</button>
+      <button className={filtro === 'maquetado'? 'activa' : ''} onClick={() => handleFiltroChange("maquetado")}>Maquetación</button>
       <button className={filtro === 'React'? 'activa' : ''} onClick={() => handleFiltroChange("React")}>React</button>
       <button className={filtro === 'JS'? 'activa' : ''} onClick={() => handleFiltroChange("JS")}>JavaScript</button>
       <button className={filtro === ''? 'activa' : ''} onClick={() => handleFiltroChange("")}>Todo</button>
